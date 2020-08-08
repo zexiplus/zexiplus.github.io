cae0dff8c2f07b746222dc8e846b3b8073a7f6e2
Float <zexiplus@outlook.com>
2020-08-08 13:04:24 +0800

    feat: remove files

--- about/about.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var arr = Array.apply(null, document.querySelectorAll('.box'))
-    arr.forEach(function (item) {
-        item.addEventListener('click', function () {
-            window.location.href = item.getAttribute('data-src')
-        })
-    })
-    
-})
